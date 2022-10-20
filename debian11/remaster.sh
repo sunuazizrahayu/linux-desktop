a777fa1670fe4aeb01a2709f9daa0d1284b351f0
sunuazizrahayu <sunuazizrahayu@gmail.com>
2022-10-20 16:57:05 +0700

    Create remaster.sh

---
+++ debian11/remaster.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+## check for sudo/root
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must run with sudo, try again..."
+  exit 1
+fi
+
+
+printf "Configure touchpad.."
+rm /etc/X11/xorg.conf.d/99-libinput.conf > /dev/null
+cat << EOF > /etc/X11/xorg.conf.d/99-libinput.conf
+Section "InputClass"
+    Identifier "libinput touchpad catchall"
+    MatchIsTouchpad "on"
+    Driver "libinput"
+    Option "Tapping" "True"
+    Option "NaturalScrolling" "True"
+EndSection
+EOF
