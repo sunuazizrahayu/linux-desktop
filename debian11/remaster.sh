df4067225af8cb495404c58e36f475a9aa24a8f5
sunuazizrahayu <sunuazizrahayu@gmail.com>
2022-10-24 16:00:28 +0700

    Update remaster.sh

--- debian11/remaster.sh
+++ debian11/remaster.sh
@@ -18,6 +18,12 @@ printf "\nInstalling Microsoft Fonts\n"
 apt install \
   ttf-mscorefonts-installer \
   -y
+wget -q -O - https://gist.githubusercontent.com/Blastoise/72e10b8af5ca359772ee64b6dba33c91/raw/2d7ab3caa27faa61beca9fbf7d3aca6ce9a25916/clearType.sh | bash
+wget -q -O - https://gist.githubusercontent.com/Blastoise/b74e06f739610c4a867cf94b27637a56/raw/96926e732a38d3da860624114990121d71c08ea1/tahoma.sh | bash
+wget -q -O - https://gist.githubusercontent.com/Blastoise/64ba4acc55047a53b680c1b3072dd985/raw/6bdf69384da4783cc6dafcb51d281cb3ddcb7ca0/segoeUI.sh | bash
+wget -q -O - https://gist.githubusercontent.com/Blastoise/d959d3196fb3937b36969013d96740e0/raw/429d8882b7c34e5dbd7b9cbc9d0079de5bd9e3aa/otherFonts.sh | bash
+mv ~/.fonts/ /usr/share/fonts/
+fc-cache -f -v
 
 
 printf "\n\n\nConfigure touchpad..\n"
