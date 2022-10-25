12726bb0d415df9ca43bed8225bbf7c38736af4f
sunuazizrahayu <sunuazizrahayu@gmail.com>
2022-10-25 09:23:47 +0700

    Update install-software.sh

--- debian11/install-software.sh
+++ debian11/install-software.sh
@@ -11,7 +11,7 @@ printf "Add Sublime repo...\n"
 rm /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 rm /etc/apt/sources.list.d/sublime-text.list
 wget -qO- https://download.sublimetext.com/sublimehq-pub.gpg | sudo gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list > /dev/null
 apt update && apt install sublime-text sublime-merge -y
 
 
@@ -19,24 +19,32 @@ printf "Add VSCode...\n"
 rm /etc/apt/trusted.gpg.d/vscode.gpg
 rm /etc/apt/sources.list.d/vscode.list
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/vscode.gpg > /dev/null
-echo "deb [arch=$(dpkg --print-architecture)] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+echo "deb [arch=$(dpkg --print-architecture)] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 apt update && apt install code -y
 
 
 printf "Beekeeper SQL Editor..\n"
 rm /etc/apt/trusted.gpg.d/beekeeper.gpg
 rm /etc/apt/sources.list.d/beekeeper-studio-app.list
-wget -qO- https://deb.beekeeperstudio.io/beekeeper.key | sudo gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/beekeeper.gpg
-echo 'deb https://deb.beekeeperstudio.io stable main' | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list
+wget -qO- https://deb.beekeeperstudio.io/beekeeper.key | sudo gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/beekeeper.gpg > /dev/null
+echo 'deb https://deb.beekeeperstudio.io stable main' | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list > /dev/null
 apt update && apt install beekeeper-studio -y
 
 
 printf "Install Docker...\n"
 rm /etc/apt/trusted.gpg.d/docker.gpg
 rm /etc/apt/sources.list.d/docker.list
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg  > /dev/null
+echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list  > /dev/null
 apt update && apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose -y
+
+
+printf "\nInstall Penguin"
+rm /etc/apt/trusted.gpg.d/penguins-eggs.gpg
+rm /etc/apt/sources.list.d/penguins-eggs.list
+curl -fsSL https://pieroproietti.github.io/penguins-eggs-ppa/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/penguins-eggs.gpg
+echo "deb [arch=$(dpkg --print-architecture)] https://pieroproietti.github.io/penguins-eggs-ppa ./" | sudo tee /etc/apt/sources.list.d/penguins-eggs.list > /dev/null
+apt update && apt install eggs -y
 
 
 printf "\nUpdating repo list..\n"
