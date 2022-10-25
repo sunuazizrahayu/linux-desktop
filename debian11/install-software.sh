fa613e32fe76eb05746abb69eebdd56ba019f81c
sunuazizrahayu <sunuazizrahayu@gmail.com>
2022-10-25 09:06:55 +0700

    Update install-software.sh

--- debian11/install-software.sh
+++ debian11/install-software.sh
@@ -10,7 +10,7 @@ fi
 printf "Add Sublime repo...\n"
 rm /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 rm /etc/apt/sources.list.d/sublime-text.list
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+wget -qO- https://download.sublimetext.com/sublimehq-pub.gpg | sudo gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 apt update && apt install sublime-text sublime-merge -y
 
