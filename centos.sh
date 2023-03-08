e2787989f01b67bd99b6603119bedcc11c3b092f
sunuazizrahayu <sunuazizrahayu@gmail.com>
2023-03-08 13:43:21 +0700

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -7,33 +7,26 @@ YELLOW='\033[1;33m'
 
 
 echo -e "${YELLOW}Updating repo...${NC}"
-sudo yum check-update
+yum check-update
 echo ""
 
 echo -e "${YELLOW}Removing Apache...${NC}"
-sudo yum erase -y httpd httpd-tools apr apr-util
+yum erase -y httpd httpd-tools apr apr-util
 echo ""
-
-echo -e "${YELLOW}Upgrade...${NC}"
-sudo yum check-update
-sudo yum upgrade -y
-echo ""
-
-
 
 echo -e "${YELLOW}Install Requirements...${NC}"
-sudo yum install -y epel-release
-sudo yum install -y nano curl wget bash-completion htop
+yum install -y epel-release
+yum install -y nano curl wget bash-completion htop
 echo ""
 
 echo -e "${YELLOW}Install NGINX...${NC}"
-sudo yum install -y nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
+yum install -y nginx
+systemctl start nginx
+systemctl enable nginx
 echo ""
 
 echo -e "${YELLOW}Installing Docker...${NC}"
-sudo yum remove -y docker \
+yum remove -y docker \
     docker-client \
     docker-client-latest \
     docker-common \
@@ -41,11 +34,11 @@ sudo yum remove -y docker \
     docker-latest-logrotate \
     docker-logrotate \
     docker-engine
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
-sudo systemctl start docker
-sudo systemctl enable docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+systemctl start docker
+systemctl enable docker
 echo ""
 
 
@@ -54,6 +47,6 @@ wget https://github.com/fscarmen/warp/raw/main/menu.sh
 wget https://github.com/sunuazizrahayu/linux-conf/raw/main/warp.py
 
 echo -e "${YELLOW}Install Speedtest...${NC}"
-curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.rpm.sh | sudo bash && sudo yum install -y speedtest
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.rpm.sh | bash && yum install -y speedtest
 
 echo -e "${YELLOW}Done...${NC}"
