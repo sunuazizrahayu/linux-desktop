e4b5ba24532230c4d55ea9b327610a040d9d6c15
sunuazizrahayu <sunuazizrahayu@gmail.com>
2023-06-16 14:46:31 +0700

    Update xfce-docklike.sh

--- debian12/xfce-docklike.sh
+++ debian12/xfce-docklike.sh
@@ -10,3 +10,10 @@ fi
 apt install xfce4-dev-tools make xorg-dev libglib2.0-dev libgtk-3-dev libwnck-3-dev \
   libxfce4ui-2-dev libxfce4panel-2.0-dev \
   g++
+
+# install
+wget https://archive.xfce.org/src/panel-plugins/xfce4-docklike-plugin/0.4/xfce4-docklike-plugin-0.4.0.tar.bz2
+tar -xvjf xfce4-docklike-plugin-0.4.0.tar.bz2 && cd xfce4-docklike-plugin-0.4.0
+./configure
+make
+sudo make install
