b150f9f90f5611b6b747607c2596b23abe1bd77e
sunuazizrahayu <sunuazizrahayu@gmail.com>
2023-06-16 14:42:42 +0700

    Update install-software.sh

--- debian12/install-software.sh
+++ debian12/install-software.sh
@@ -37,4 +37,5 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list  > /dev/null
 sudo apt update 
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
-
+sudo systemctl start docker
+sudo systemctl disable docker
