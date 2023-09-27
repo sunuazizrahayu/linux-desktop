5394f486eca63e4ef4b6037fb7b63b83b06c0667
sunuazizrahayu <sunuazizrahayu@gmail.com>
2023-09-27 16:51:11 +0700

    Update install-software.sh

--- linuxmint/install-software.sh
+++ linuxmint/install-software.sh
@@ -6,6 +6,8 @@ if ! [ $(id -u) = 0 ]; then
   exit 1
 fi
 
+
+
 # Install Sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
@@ -13,7 +15,9 @@ sudo apt update
 sudo apt install sublime-text sublime-merge -y
 
 
+
 # Install Docker
+printf "\n\nInstall Docker...\n"
 sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -25,8 +29,11 @@ echo \
 
 sudo apt update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+
 # access docker without `sudo`
+printf "\n\nAdd User Docker...\n"
 sudo usermod -aG docker $(whoami)
+
 
 
 # Firefox ESR
