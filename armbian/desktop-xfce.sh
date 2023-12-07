0eb6ea25ba98f8b629e1db997587d08939b89b22
sunuazizrahayu <sunuazizrahayu@gmail.com>
2023-12-07 16:57:29 +0700

    Create desktop-xfce.sh

---
+++ armbian/desktop-xfce.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+## check for sudo/root
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must run with sudo, try again..."
+  exit 1
+fi
+
+
+# update repository
+echo "Update Repository"
+sudo apt update
+printf "\n\n\n"
+
+
+# install requirements
+echo "Install Requirements"
+sudo apt install \
+  bash-completion \
+  wget \
+  unzip \
+  nano \
+  -y
+printf "\n\n\n"
+
+
+# xfce
+echo "Install XFCE minimal"
+sudo apt install \
+  xfce4-appfinder \
+  xfce4-panel \
+  xfce4-session \
+  xfce4-settings \
+  xfce4-terminal \
+  xfconf \
+  xfdesktop4 \
+  xfwm4 \
+  thunar \
+  -y
+printf "\n\n\n"
+
+echo "Install XFCE Requirements"
+sudo apt install \
+  xorg \
+  lightdm \
+  -y
+printf "\n\n\n"
+
