fe8e238507f54238a57b97f5678acdb2ac56da83
sunuazizrahayu <sunuazizrahayu@gmail.com>
2024-09-13 16:45:57 +0700

    Update install-software.sh

--- debian12/install-software.sh
+++ debian12/install-software.sh
@@ -12,14 +12,6 @@ apt install wget curl \
   htop \
   -y
 
-
-printf "\nAdd VitexSoftware repo...\n"
-rm /etc/apt/trusted.gpg.d/vitexsoftware.gpg
-rm /etc/apt/sources.list.d/vitexsoftware.list
-wget -qO- https://repo.vitexsoftware.com/keyring.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/vitexsoftware.gpg > /dev/null
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/vitexsoftware.gpg] https://repo.vitexsoftware.com bookworm main" | sudo tee /etc/apt/sources.list.d/vitexsoftware.list
-
-
 printf "\nAdd Sublime repo...\n"
 rm /etc/apt/trusted.gpg.d/sublimehq-archive.gpg
 rm /etc/apt/sources.list.d/sublime-text.list
