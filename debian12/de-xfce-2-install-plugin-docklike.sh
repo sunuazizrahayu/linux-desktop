77b5c81a2e2e29cccf90b3f925b84f9e9a71a939
sunuazizrahayu <sunuazizrahayu@gmail.com>
2024-09-14 07:06:47 +0700

    Update de-xfce-2-install-plugin-docklike.sh

--- debian12/de-xfce-2-install-plugin-docklike.sh
+++ debian12/de-xfce-2-install-plugin-docklike.sh
@@ -14,11 +14,13 @@ sudo apt install \
   -y
 
 # install
-wget https://archive.xfce.org/src/panel-plugins/xfce4-docklike-plugin/0.4/xfce4-docklike-plugin-0.4.0.tar.bz2
-tar -xvjf xfce4-docklike-plugin-0.4.0.tar.bz2 && cd xfce4-docklike-plugin-0.4.0
+current_dir=$(pwd)
+wget https://archive.xfce.org/src/panel-plugins/xfce4-docklike-plugin/0.4/xfce4-docklike-plugin-0.4.0.tar.bz2 -P /tmp
+tar -xvjf /tmp/xfce4-docklike-plugin-0.4.0.tar.bz2 && cd /tmp/xfce4-docklike-plugin-0.4.0
 ./configure
 make
 sudo make install
+cd "$current_dir"
 
 sudo apt purge \
   xfce4-dev-tools make xorg-dev libglib2.0-dev libgtk-3-dev libwnck-3-dev \
