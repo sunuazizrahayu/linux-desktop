a4a664cc11e48560a99392f40eccd5f3d38a77df
sunuazizrahayu <sunuazizrahayu@gmail.com>
2025-12-01 14:55:50 +0700

    Add OnlyOffice installation script for Linux Mint

--- linuxmint-de7/onlyoffice/onlyoffice-install.sh
+++ linuxmint-de7/onlyoffice/onlyoffice-install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# install onlyoffice
 wget -qO- https://github.com/sunuazizrahayu/linux-desktop/raw/refs/heads/main/linuxmint-de7/onlyoffice/onlyoffice.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/onlyoffice.gpg > /dev/null
 echo 'deb [signed-by=/usr/share/keyrings/onlyoffice.gpg] https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
+sudo apt update
+sudo apt install onlyoffice-desktopeditors -y
