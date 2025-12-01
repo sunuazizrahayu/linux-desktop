4833443eb3df37c519b84df09f8bca9f3d4f90af
sunuazizrahayu <sunuazizrahayu@gmail.com>
2025-12-01 15:36:53 +0700

    Create install-software2.sh
    This script installs DBeaver by adding its GPG key and repository.

---
+++ linuxmint-de7/install-software2.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# dbeaver
+sudo  wget -O /usr/share/keyrings/dbeaver.gpg.key https://dbeaver.io/debs/dbeaver.gpg.key
+echo "deb [signed-by=/usr/share/keyrings/dbeaver.gpg.key] https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+sudo apt update && sudo apt install dbeaver-ce -y
